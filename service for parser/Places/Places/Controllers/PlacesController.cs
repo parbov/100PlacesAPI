ea6f65dc14752d6e7dabfb883fd68418f1caab26
parbov <parbov@gmail.com>
2013-09-01 17:52:16 +0300

    ready service

--- service for parser/Places/Places/Controllers/PlacesController.cs
+++ service for parser/Places/Places/Controllers/PlacesController.cs
@@ -16,7 +16,7 @@ namespace Places.Controllers
         [ActionName("get-place")]
         public string GetPlace(string url)
         {
-            var site = "http://100obekta.com/en/%D0%BD%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D0%BD%D0%B0-%D1%85%D1%83%D0%B4%D0%BE%D0%B6%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%B0-%D0%B3%D0%B0%D0%BB%D0%B5%D1%80%D0%B8%D1%8F/";
+            var site = "http://100obekta.com/en/"+url;
             var client = new WebClient();
             var htmlText = client.DownloadString(site);
 
